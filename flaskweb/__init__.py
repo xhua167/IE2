43e264e256cb6bd60a086dfb156f9e32e2ee8b08
Leno <42096622+xhua167@users.noreply.github.com>
2019-10-14 19:11:05 +1100

    Update __init__.py

--- flaskweb/__init__.py
+++ flaskweb/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4d7e72400c6b35d23f38678282d3ba5d'
-uri = 'mongodb://umbsupportdb:1xcxH2Teq0O02cp8S7Pj4XSwXd0vqUgd56l9jPiWVm0ewsO0qTUPfNA9YHtoIBQMVmrP9M3twmyReKLff2NuoQ==@umbsupportdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
+uri = 'mongodb://lenomongo:kzUR8Q9x2b0CSbxp8A0lpVpXPIl07VEnwhCDlrKZLv7imd7Ik0B6O5tpZk6khUemBbDqH8OgdTaAxQxFdtrRdw==@lenomongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 client = MongoClient(uri)
 db = client.IE
 bcrypt = Bcrypt(app)
