d85c33b175055ff1cef5326df4a2073c8d29e1b4
Leno <42096622+xhua167@users.noreply.github.com>
2019-10-20 18:05:31 +1100

    Update __init__.py

--- flaskweb/__init__.py
+++ flaskweb/__init__.py
@@ -12,7 +12,8 @@ app.config['BASIC_AUTH_PASSWORD'] = 'help1024'
 app.config['BASIC_AUTH_FORCE'] = True
 basic_auth = BasicAuth(app)
 
-uri = 'mongodb://lenomongo:kzUR8Q9x2b0CSbxp8A0lpVpXPIl07VEnwhCDlrKZLv7imd7Ik0B6O5tpZk6khUemBbDqH8OgdTaAxQxFdtrRdw==@lenomongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
+uri = 'mongodb://lenomongo726:cN1mZVS8a0fIOZVm4dvZwGCmpgnWNeah0YSsFZ8DuyTwmpJWC9Z7k05WVvEPSvhk26Lt0QnBlP4P7lxofJ7Rzw==@lenomongo726.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
+
 client = MongoClient(uri)
 db = client.IE
 bcrypt = Bcrypt(app)
